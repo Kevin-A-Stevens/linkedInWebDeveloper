4ea6093158ecc69877e3b834ee51e76cb0f7dfa3
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-12-18 14:22:43 -0600

    JavaScript Exercise files remove

--- 3_JAVASCRIPT/Exercise Files/04_05/script.js
+++
@@ -1,8 +0,0 @@
-/* Creating an Annymous Function */
-var theBiggest = function(a,b) {
-  var result;
-  a>b ? result = ["a", a] : result = ["b", b];
-  return result;
-}
-
-console.log(theBiggest(7/9,13/25));
